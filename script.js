58beb068b4d5e6c4691fdfd5a305720b03cd6ae3
ErikServantes <116036335+ErikServantes@users.noreply.github.com>
2024-05-18 02:39:39 +0100

    Update script.js

--- script.js
+++ script.js
@@ -40,13 +40,13 @@ function createCard(data) {
         <div class="subtitulo">${data[2].trim()}</div>
         <div class="ilustracao" style="background-image: url('${illustrationPath}');"></div>
         <div class="atributos">
-            <div class="atributo" style="background-color: rgba(128, 179, 237, 0.5);">
+            <div class="atributo" style="background-color: rgba(128, 179, 237, 0.25);">
                 &#9876; ${data[4].trim()}
             </div>
-            <div class="atributo" style="background-color: rgba(232, 64, 64, 0.5);">
+            <div class="atributo" style="background-color: rgba(232, 64, 64, 0.25);">
                 &#9829; ${data[5].trim()}
             </div>
-            <div class="atributo" style="background-color: rgba(232, 234, 170, 0.5);">
+            <div class="atributo" style="background-color: rgba(232, 234, 170, 0.25);">
                 &#x3df; ${data[6].trim()}
             </div>
         </div>
