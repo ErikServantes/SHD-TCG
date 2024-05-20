7bd46bab82ccbb0a31ef663b2e096d8356609644
ErikServantes <116036335+ErikServantes@users.noreply.github.com>
2024-05-20 09:24:16 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,32 +1,7 @@
-document.addEventListener('DOMContentLoaded', function() {
-    fetch('cards.csv')
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok: ' + response.statusText);
-            }
-            return response.text();
-        })
-        .then(data => {
-            const rows = data.split('\n').slice(1);
-            const container = document.getElementById('card-container');
-            rows.forEach(row => {
-                if (row.trim() !== '') {
-                    const columns = row.split(',');
-                    if (columns.length === 12) { // Verifica se a linha tem o número correto de colunas
-                        const card = createCard(columns);
-                        container.appendChild(card);
-                    }
-                }
-            });
-        })
-        .catch(error => {
-            console.error('Error fetching or processing the CSV file:', error);
-        });
-});
-
 function createCard(data) {
     const imagePath = `images/${data[3].trim()}.png`;
     const illustrationPath = `images/${data[10].trim()}.png`;
+    const logoPath = `images/${data[3].trim()}logo.png`; // Caminho do logotipo
 
     const card = document.createElement('div');
     card.className = 'card';
@@ -62,20 +37,10 @@ function createCard(data) {
         <div class="id-container" style="background-color: ${rarityColor};">
             <div class="id">${data[10].trim()}</div>
         </div>
+        <div class="logo-container">
+            <img src="${logoPath}" alt="Logo">
+        </div>
     `;
 
     return card;
-}
-
-function getColorByRarity(rarity) {
-    switch(rarity) {
-        case 'R':
-            return 'gold';
-        case 'I':
-            return 'silver';
-        case 'C':
-            return 'white';
-        default:
-            return 'black';
-    }
 }
