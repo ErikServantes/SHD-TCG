126772ccb84424ed4d92845df839b5d10844cb0e
ErikServantes <116036335+ErikServantes@users.noreply.github.com>
2024-05-21 15:59:18 +0100

    Update script.js

--- script.js
+++ script.js
@@ -19,41 +19,45 @@ document.addEventListener('DOMContentLoaded', function() {
         });
 
     function populateFilters(cards) {
-        const culturas = [...new Set(cards.map(card => card[3]))];
-        const tipos = [...new Set(cards.map(card => card[7]))];
-        
+        const culturas = [...new Set(cards.map(card => card[3]).filter(cultura => cultura))];
+        const tipos = [...new Set(cards.map(card => card[7]).filter(tipo => tipo))];
+
         const culturaFilters = document.getElementById('cultura-filters');
         culturas.forEach(cultura => {
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.value = cultura;
-            checkbox.id = `cultura-${cultura}`;
-            checkbox.addEventListener('change', () => filterAndSortCards());
+            if (cultura) { // Verifica se a cultura não é vazia
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.value = cultura;
+                checkbox.id = `cultura-${cultura}`;
+                checkbox.addEventListener('change', () => filterAndSortCards());
 
-            const label = document.createElement('label');
-            label.htmlFor = `cultura-${cultura}`;
-            label.textContent = cultura;
+                const label = document.createElement('label');
+                label.htmlFor = `cultura-${cultura}`;
+                label.textContent = cultura;
 
-            culturaFilters.appendChild(checkbox);
-            culturaFilters.appendChild(label);
-            culturaFilters.appendChild(document.createElement('br'));
+                culturaFilters.appendChild(checkbox);
+                culturaFilters.appendChild(label);
+                culturaFilters.appendChild(document.createElement('br'));
+            }
         });
 
         const tipoFilters = document.getElementById('tipo-filters');
         tipos.forEach(tipo => {
-            const checkbox = document.createElement('input');
-            checkbox.type = 'checkbox';
-            checkbox.value = tipo;
-            checkbox.id = `tipo-${tipo}`;
-            checkbox.addEventListener('change', () => filterAndSortCards());
+            if (tipo) { // Verifica se o tipo não é vazio
+                const checkbox = document.createElement('input');
+                checkbox.type = 'checkbox';
+                checkbox.value = tipo;
+                checkbox.id = `tipo-${tipo}`;
+                checkbox.addEventListener('change', () => filterAndSortCards());
 
-            const label = document.createElement('label');
-            label.htmlFor = `tipo-${tipo}`;
-            label.textContent = tipo;
+                const label = document.createElement('label');
+                label.htmlFor = `tipo-${tipo}`;
+                label.textContent = tipo;
 
-            tipoFilters.appendChild(checkbox);
-            tipoFilters.appendChild(label);
-            tipoFilters.appendChild(document.createElement('br'));
+                tipoFilters.appendChild(checkbox);
+                tipoFilters.appendChild(label);
+                tipoFilters.appendChild(document.createElement('br'));
+            }
         });
 
         document.getElementById('sort-by').addEventListener('change', () => filterAndSortCards());
@@ -106,59 +110,59 @@ document.addEventListener('DOMContentLoaded', function() {
         renderCards(cardsData);
     }
 
-function createCard(data, scale = 1) {
-    const imagePath = `images/${data[3].trim()}.png`;
-    const illustrationPath = `images/${data[10].trim()}.png`;
-    const logoPath = `images/${data[3].trim()}logo.png`; // Caminho do logotipo
+    function createCard(data, scale = 1) {
+        const imagePath = `images/${data[3].trim()}.png`;
+        const illustrationPath = `images/${data[10].trim()}.png`;
+        const logoPath = `images/${data[3].trim()}logo.png`; // Caminho do logotipo
 
-    const card = document.createElement('div');
-    card.className = 'card';
-    card.style.backgroundImage = `url('${imagePath}')`;
-    card.style.transform = `scale(${scale})`;
+        const card = document.createElement('div');
+        card.className = 'card';
+        card.style.backgroundImage = `url('${imagePath}')`;
+        card.style.transform = `scale(${scale})`;
 
-    const rarityColor = getColorByRarity(data[11].trim());
+        const rarityColor = getColorByRarity(data[11].trim());
 
-    card.innerHTML = `
-        <div class="custo-container">
-            <div class="custo">${data[0].trim()}</div>
-        </div>
-        <div class="titulo-subtitulo-container">
-            <div class="titulo">${data[1].trim()}</div>
-            <div class="subtitulo">${data[2].trim()}</div>
-        </div>
-        <div class="ilustracao" style="background-image: url('${illustrationPath}');"></div>
-        <div class="atributos">
-            <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
-                ${data[4].trim()} <span class="simbolo">&#9876;</span>
+        card.innerHTML = `
+            <div class="custo-container">
+                <div class="custo">${data[0].trim()}</div>
             </div>
-            <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
-                ${data[5].trim()} <span class="simbolo">&#9829;</span>
+            <div class="titulo-subtitulo-container">
+                <div class="titulo">${data[1].trim()}</div>
+                <div class="subtitulo">${data[2].trim()}</div>
             </div>
-            <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
-                ${data[6].trim()} <span class="simbolo">&#x3df;</span>
+            <div class="ilustracao" style="background-image: url('${illustrationPath}');"></div>
+            <div class="atributos">
+                <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
+                    ${data[4].trim()} <span class="simbolo">&#9876;</span>
+                </div>
+                <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
+                    ${data[5].trim()} <span class="simbolo">&#9829;</span>
+                </div>
+                <div class="atributo" style="background-color: rgba(217, 203, 128, 0.9);">
+                    ${data[6].trim()} <span class="simbolo">&#x3df;</span>
+                </div>
             </div>
-        </div>
-        <div class="tipo">${data[7].trim()}</div>
-        <div class="texto-lore-container">
-            <div class="texto">${data[8].trim()}</div>
-            <div class="lore">${data[9].trim()}</div>
-        </div>
-        <div class="id-container" style="background-color: ${rarityColor};">
-            <div class="id">${data[10].trim()}</div>
-        </div>
-        <div class="logo-container">
-            <img src="${logoPath}" alt="Logo">
-        </div>
-        <div class="novo-container">
-            <img src="${logoPath}" alt="Logo Novo">
-        </div>
-        <div class="count" style="display: none;">x1</div>
-    `;
+            <div class="tipo">${data[7].trim()}</div>
+            <div class="texto-lore-container">
+                <div class="texto">${data[8].trim()}</div>
+                <div class="lore">${data[9].trim()}</div>
+            </div>
+            <div class="id-container" style="background-color: ${rarityColor};">
+                <div class="id">${data[10].trim()}</div>
+            </div>
+            <div class="logo-container">
+                <img src="${logoPath}" alt="Logo">
+            </div>
+            <div class="novo-container">
+                <img src="${logoPath}" alt="Logo Novo">
+            </div>
+            <div class="count" style="display: none;">x1</div>
+        `;
 
-    // Adiciona os eventos de mouse
-    addPopupEvents(card, data);
+        // Adiciona os eventos de mouse
+        addPopupEvents(card, data);
 
-    return card;
+        return card;
     }
 
     function addToDeck(data, cardCount) {
